d8a2cc6d3e141e16e593aa4fc22e5d2b6ec46b03
Cleber Severo <clebersevero1998@gmail.com>
2025-07-26 18:17:37 -0300

    chore: add header dropdown menu

---
+++ src/components/account-menu.tsx
@@ -0,0 +1,34 @@
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu'
+import { Button } from './ui/button'
+import { Building, ChevronDown, LogOut } from 'lucide-react'
+
+const AccountMenu = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={'outline'} className='flex items-center gap-2 select-none'>
+          Pizza shop
+          <ChevronDown className='w-4 h-4' />
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent align='end' className='w-56'>
+        <DropdownMenuLabel className='flex flex-col'>
+          <span>Cléber Severo</span>
+          <span className='text-xs font-normal text-muted-foreground'>clebersevero@gmail.com</span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Building className='w-4 h-4 mr-2' />
+          <span>Perfil da loja</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className='text-rose-500 dark:text-rose-400'>
+          <LogOut className='w-4 h-4 mr-2 text-rose-500 dark:text-rose-400' />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+export default AccountMenu
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,8 @@
 import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
 import NavLink from './NavLink'
 import { Separator } from './ui/separator'
+import { ThemeToggle } from './theme/theme-toggle'
+import AccountMenu from './account-menu'
 
 const Header = () => {
   return (
@@ -20,6 +22,11 @@ const Header = () => {
             Pedidos
           </NavLink>
         </nav>
+
+        <div className='ml-auto flex items-center gap-2'>
+          <ThemeToggle />
+          <AccountMenu />
+        </div>
       </div>
     </div>
   )
