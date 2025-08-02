09393045f4c55e2763e6775daf37d8d5720754f9
Cleber Severo <clebersevero1998@gmail.com>
2025-08-02 08:13:40 -0300

    fix: removing unused console.log

--- src/pages/auth/sing-in.tsx
+++ src/pages/auth/sing-in.tsx
@@ -31,10 +31,7 @@ export function SignIn() {
 
   async function handleSignIn(data: SignInForm) {
     try {
-      console.log(data)
       await authenticate({ email: data.email })
-      // await new Promise((resolve) => setTimeout(resolve, 2000))
-
 
       toast.success('Enviamos um link de autenticação para seu e-mail.', {
         action: {
