3966fda9260313fc893e08905d5e015b131804fa
Cleber Severo <clebersevero1998@gmail.com>
2025-08-02 07:12:52 -0300

    feat: signup

--- src/pages/auth/sing-up.tsx
+++ src/pages/auth/sing-up.tsx
@@ -1,6 +1,8 @@
+import { registerRestaurant } from '@/api/register-restaurant';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@radix-ui/react-label';
+import { useMutation } from '@tanstack/react-query';
 import { Helmet, } from 'react-helmet-async';
 import { useForm } from 'react-hook-form'
 import { Link, useNavigate } from 'react-router-dom';
@@ -16,20 +18,30 @@ const signUpForm = z.object({
 
 type SignUpForm = z.infer<typeof signUpForm>
 
+
 export function SignUp() {
   const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignUpForm>()
 
   const navigate = useNavigate()
 
+  const { mutateAsync: registerRestaurantFn } = useMutation({
+    mutationFn: registerRestaurant,
+  })
+
   async function handleSignUp(data: SignUpForm) {
     try {
-      console.log(data)
-      await new Promise((resolve) => setTimeout(resolve, 2000))
+
+      registerRestaurantFn({
+        email: data.restaurantName,
+        managerName: data.managerName,
+        phone: data.phone,
+        restaurantName: data.restaurantName,
+      })
 
       toast.success('Restaurante cadastrado com sucesso.', {
         action: {
           label: 'Login',
-          onClick: () => navigate('/sign-in')
+          onClick: () => navigate(`/sign-in?email=${data.email}`)
         }
       })
     } catch (error) {
